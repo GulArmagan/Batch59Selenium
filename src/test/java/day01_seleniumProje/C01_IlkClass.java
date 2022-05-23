e62e4bc29ce86cad51db5d0d69ef22398712605a
GulArmagan <g.armagan.kurtoglu@gmail.com>
2022-05-23 17:43:17 +0300

    first commit

---
+++ src/test/java/day01_seleniumProje/C01_IlkClass.java
@@ -0,0 +1,16 @@
+package day01_seleniumProje;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_IlkClass {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src\\resources\\drives\\chromedriver.exe");
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+
+
+    }
+
+}
